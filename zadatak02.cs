d2ad0fb6914b3e0d6e55c43b69b624e30a58e6f6
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2023-12-12 12:32:04 +0100

    Add files via upload

---
+++ zadatak02.cs
@@ -0,0 +1,17 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a = 10;
+		int b = 7;
+		double c = 12.5;
+		double d = 16.7;
+		double x;
+		x=(a+b+c+d)/4;
+			
+			Console.WriteLine("rezultat je: {0}",+x);
+		
+	}
+}
